b46826a3daaf92c898d2fca7adb4d3508890d4f4
Nathan Glasl <nathan@silverstripe.com.au>
2016-07-29 14:39:39 +1000

    Updating the script so it's now possible to pass through a list of packagist owners. This also purges some redundant code/flags, and introduces some new ones. This will be documented further in the next round of updates.

---
+++ SSAU_supported_modules.php
@@ -0,0 +1,24 @@
+<?php
+
+// The core SSAU supported packagist modules.
+
+$supported = array(
+	'silverstripe/advancedworkflow',
+	'silverstripe/alchemiser',
+	'silverstripe/cdncontent',
+	'silverstripe/content-services',
+	'silverstripe/external-content',
+	'silverstripe/frontend-dashboards',
+	'silverstripe/listingpage',
+	'silverstripe/microblog',
+	'silverstripe/multivaluefield',
+	'silverstripe/queuedjobs',
+	'silverstripe/restrictedobjects',
+	'silverstripe/s3cdn',
+	'silverstripe/solr',
+	'silverstripe/syncrotron',
+	'silverstripe/usertemplates',
+	'silverstripe/versionedfiles',
+	'silverstripe/webservices'
+);
+return $supported;
